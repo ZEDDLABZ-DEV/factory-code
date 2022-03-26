e952d3c834667bbbe179c5d3526c83ca72f54432
Manas Shukla <shuklamanasofficial@gmail.com>
2022-03-26 13:54:00 +0530

    Initial Commit with them file

---
+++ src/theme/colors.js
@@ -0,0 +1,8 @@
+export const colors = {
+  mainPrimary: "#DDFFF5",
+  mainSecondary: "#FFFFFF",
+  mainText1: "#434343",
+  mainText2: "#263238",
+  primaryBtn: "#00B983",
+  mainCard: "#455A64",
+};
---
+++ src/theme/text.js
@@ -0,0 +1,16 @@
+export const fontSize = {
+  sm: "16px",
+  md: "20px",
+  lg: "24px",
+  xl: "28px",
+  "2xl": "32px",
+  h1: "36px",
+};
+
+export const fontWeight = {
+  light: "300",
+  normal: "400",
+  semiBold: "500",
+  bold: "600",
+  extraBold: "700",
+};
