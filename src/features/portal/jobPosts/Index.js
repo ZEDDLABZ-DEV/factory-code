b2f547bb3bd27f1d386982d4ab50d23484bd6f5d
iamLucidDreamer <shuklamanasofficial@gmail.com>
2022-04-22 11:23:36 -0700

    Live Job Posts Thing Added

--- src/features/portal/jobPosts/Index.js
+++ src/features/portal/jobPosts/Index.js
@@ -3,68 +3,42 @@ import axios from "axios";
 import React, { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import call from "../../../utils/call";
-// import call from "../../../utils/call";
+import JobPostCard from "./components/JobPostCard";
+
 
 const JobPosts = () => {
-  const [data, setData] = useState([]);
+  const [jobPostsData, setJobPostsData] = useState([]);
 
   const getData = () => {
     call({
-      url: "https://apitest.aamdhane.com/api/job?size=100",
+      url: "/api/job?size=100",
       type: "GET",
     })
-      .then((res) => setData(res?.data))
+      .then((res) => setJobPostsData(res))
       .catch(() => {});
   };
   useEffect(() => getData(), []);
-  const navigate = useNavigate();
-  const columns = [
-    {
-      key: "",
-      title: "Heading 1",
-      render: (data) => <p>{data?.id}</p>,
-    },
-    {
-      key: "",
-      title: "Heading 2",
-      render: (data) => <p>{data?.id}</p>,
-    },
-    {
-      key: "",
-      title: "Heading 3",
-    },
-    {
-      key: "",
-      title: "Heading 4",
-    },
-    {
-      key: "",
-      title: "Heading 5",
-    },
-    {
-      key: "",
-      title: "ACtions",
-      width: "auto",
-      render: () => (
-        <div className="felx-flex-row">
-          <Button type="primary">Action1</Button>
-          <Button type="primary">Action1</Button>
-          <Button type="primary">Action1</Button>
-        </div>
-      ),
-    },
-  ];
+
+  console.log(jobPostsData)
+
   return (
     <div className="px-12 pt-10 flex flex-col h-full">
-      <h1 className="text-h1 text-mainDashboard font-extraBold leading-6 mb-8">
-        Job Posts
+      <div className="flex items-center justify-between mb-8">
+      <h1 className="text-h1 text-mainDashboard font-extraBold leading-6 ">
+        Live Job Posts
       </h1>
-      <div>
-          <Table columns={columns} dataSource={data} />
+      <Button>Create New</Button>
       </div>
-      <Button onClick={() => navigate("/portal/jobPosts/create")}>
-        Create new Job Post
-      </Button>
+      {jobPostsData?.map((data) => (
+          <JobPostCard
+            key={data.id}
+            jobTitle={data.jobTitle}
+            skills={data.skillReq}
+            shiftSalary={data.salaryPerShift}
+            expReq={data.expReq}
+            reqWorkers={data.peopleReq}
+          />
+        ))}
     </div>
   );
 };
